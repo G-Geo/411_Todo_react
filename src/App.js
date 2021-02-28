065ab0078287f8bbe589ee82ff46d53197c3bf00
Cody-Blair <cb.rialbc@gmail.com>
2021-02-28 15:14:17 -0600

    Got remove button working

--- src/App.js
+++ src/App.js
@@ -1,25 +1,66 @@
+import REACT, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+  constructor(props){
+    super(props)
+
+    this.state = {
+        isClicked: false,
+        inputValue: "",
+        listOfTodos: []
+      }
+  }
+
+  handleChange = (event) => {
+    this.setState({inputValue: event.target.value})
+  }
+  handleSubmit = (event) => {
+    event.preventDefault()
+    this.setState({listOfTodos:[...this.state.listOfTodos, this.state.inputValue]})
+    this.setState({inputValue: ""})
+  }
+  handleRemove = (event) => {
+    console.log(event.target.attributes.index.value)
+    let thisIndex = event.target.attributes.index.value
+    this.state.listOfTodos.splice(thisIndex,1)
+    this.setState({listOfTodos:[...this.state.listOfTodos]})
+  }
+
+  render () {
+      return (
+      <div className="App">
+        <header className="App-header">
+          <img src={logo} className="App-logo" alt="logo" />
+  
+          <form onSubmit={this.handleSubmit}>
+            <input type ='text' value={this.state.inputValue} onChange={this.handleChange} ></input>
+            <button type="submit">Submit</button>
+          </form>
+
+          <ol>{this.state.listOfTodos.map((item, index) => {
+
+            return <div key={index} index={index} className="lidiv"> 
+                      <li>{item}</li>
+                      <button index={index} onClick={this.handleRemove}>Completed</button>
+                    </div>
+
+          })}</ol>
+          <a
+            className="App-link"
+            href="https://reactjs.org"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            {this.props.name}
+          </a>
+        </header>
+      </div>
+   );
+  }
 }
+
+  
 
 export default App;
--- src/index.js
+++ src/index.js
@@ -6,7 +6,7 @@ import reportWebVitals from './reportWebVitals';
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <App name="List of Todos"/>
   </React.StrictMode>,
   document.getElementById('root')
 );
